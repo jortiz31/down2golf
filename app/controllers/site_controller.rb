5da73d1215fd4024bd0c4d75b280ff2da17ccfd6
Jonathan <jortiz@tunein.com>
2016-05-27 12:09:55 -0700

    before installing foundation

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,4 @@
+class SiteController < ApplicationController
+  def angular
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  get 'site/angular'
+
   root 'site#angular'
+
+  scope '/api', defaults: { format: :json } do
+
+    resources :courses, except: [:new, :edit] do
+      resources :matches, except: [:new, :edit]
+    end
+  end
 
   get '*path', to: 'site#angular'
 end
