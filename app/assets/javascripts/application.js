f82c03409a4d43cd26e9157c0f3170be49a24e66
scoobaroo <scoobaroo415@gmail.com>
2016-05-27 18:25:02 -0700

    app.js

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,11 @@
+//= require angular
+//= require angular-route
+//= require angular-resource
+//= require angular-rails-templates
+//
+// Templates in app/assets/templates
+//= require_tree ../templates
+//= require listly.module
+//= require_tree .
+
+console.log('application.js loaded!');
