faec312961a54ebf0fa5e1a845e0fa1e1f710232
scoobaroo <scoobaroo415@gmail.com>
2016-05-27 17:13:38 -0700

    angular routing added site controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
   devise_scope :user do
-    root to: "users/sessions#new"
     get 'sign_in', to: 'users/sessions#new'
     get "/login", :to => "users/sessions#new", :as => :login
     get "/signup", :to => "users/registrations#new", :as => :signup
     get "/logout", :to => "users/sessions#destroy", :as => :logout
     get '*path', to: 'users/sessions#new'
   end
+  root 'site#angular'
   resources :users
   resources :matches
   resources :courses
