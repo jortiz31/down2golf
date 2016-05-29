e64290826635ce0c8c1058847f5a98e7a479d324
scoobaroo <scoobaroo415@gmail.com>
2016-05-29 15:07:34 -0700

    courses controller updated

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -8,6 +8,18 @@ class CoursesController < ApplicationController
     @matches = @course.matches
   end
 
+  def edit
+    render :edit
+  end
+
+  def update
+    @course.update(course_params)
+  end
+
+  def destroy
+    @course.destroy
+  end
+  
   private
   def course_params
     params.require(:course).permit(:name, :location, :image, :url, :price)
