a979a8279367e2481c9b7a00a2be085deb28ff1b
scoobaroo <scoobaroo415@gmail.com>
2016-06-01 16:56:25 -0700

    premium and free features working now

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -2,11 +2,8 @@ class MatchesController < ApplicationController
   skip_before_filter :verify_authenticity_token
   before_action :set_match, only: [:show, :edit, :update, :destroy]
   def new
-
-      @match=Match.new
-      @courses=Course.all
-
-
+    @match=Match.new
+    @courses=Course.all
   end
 
   def index
@@ -21,16 +18,24 @@ class MatchesController < ApplicationController
   end
 
   def create
+    @match_count = 0
+    current_user.matches.each do |match|
+      if match.users.first == current_user
+        @match_count += 1
+      end
+    end
     @comment = Comment.new
-    if current_user.premium?
+    if current_user.premium
       @match = Match.new(match_params)
       current_user.matches << @match
-    elsif current_user.matches.count >= 5
+      redirect_to @match
+    elsif @match_count >= 5
       flash[:notice]="Sign up for premium in order to create more than 5 matches!"
       redirect_to matches_path
-    elsif current_user.matches.count < 5
+    elsif @match_count < 5
       @match=Match.new(match_params)
       current_user.matches << @match
+      redirect_to @match
     end
   end
 
