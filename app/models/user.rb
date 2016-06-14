76af5856a710de52f662890ac0027ee70ab04e4d
scoobaroo <scoobaroo415@gmail.com>
2016-06-13 21:04:24 -0700

    added omniauth.rb to config/initializers

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,11 +32,13 @@ class User < ActiveRecord::Base
     result
   end
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
     end
   end
 end
